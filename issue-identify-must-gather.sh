334db15f063aeb0253ee2ec7efdeee0b5a274212
root <ayush.garg2020@outlook.com>
2022-10-22 15:47:45 +0000

    Removed MCDPodStatus, added clusterversion and pods function. Updated all functions to print if the components are working fine and whether the MCD pod logs for degraded nodes are available or not

--- issue-identify-must-gather.sh
+++ issue-identify-must-gather.sh
@@ -16,102 +16,133 @@ MUSTGATHER=${1}
 ARGS=${#}
 
 function validate {
-	if [ ${ARGS} -eq 0 ]
-	then
-		echo "No must-gather supplied!"
-		echo "USAGE: $0 <must-gather-directory>"
-		exit 1
-	elif [ ${ARGS} -gt 1 ]
-	then
-		echo "Only must-gather should be provided!"
-		echo "USAGE: $0 <must-gather-directory>"
-		exit 1
-	elif ! [ -x "$(command -v omg)" ]
-	then
-		echo 'Error: omg command not found!' >&2
-		echo 'Visit "https://pypi.org/project/o-must-gather" for install instructions.'
-		exit 1
-	elif ! [ -x "$(command -v jq)" ]
-	then
-		echo 'Error: jq command not found!' >&2
-		echo 'Visit "https://stedolan.github.io/jq/download" for install instructions.'
-		exit 1
-	elif ! [ -x "$(command -v column)" ]
-	then
-		echo 'Error: column command not found!' >&2
-		echo '"util-linux" package needs to be installed for Red Hat based Linux Distributions.'
-		echo '"bsdmainutils" package needs to be installed for Debian based Linux Distributions.'
-		exit 1
-	else
-		rm -f ~/.omgconfig && omg use ${MUSTGATHER}
-		echo -e "\n"
-	fi
+		if [ ${ARGS} -eq 0 ]
+		then
+			echo "No must-gather supplied!"
+			echo "USAGE: $0 <must-gather-directory>"
+			exit 1
+		elif [ ${ARGS} -gt 1 ]
+		then
+			echo "Only must-gather should be provided!"
+			echo "USAGE: $0 <must-gather-directory>"
+			exit 1
+		elif ! [ -x "$(command -v omg)" ]
+		then
+			echo 'Error: omg command not found!' >&2
+			echo 'Visit "https://pypi.org/project/o-must-gather" for install instructions.'
+			exit 1
+		else
+			rm -f ~/.omgconfig && omg use ${MUSTGATHER}
+			echo -e "\n"
+		fi
 }
 
+### fetch clusterversion details
+function clusterversion {
+		omg get clusterversion
+}
+
+### Degraded operators
+function DegradedOperators {
+		DegradedOperatorsNames=$(omg get co | awk '$3!="True"||$4!="False"||$5!="False"')
+		LineCount=$(echo "$DegradedOperatorsNames" | wc -l)
+		if [ $LineCount -eq 1 ];
+		then
+			echo -e "\n${PURPLE}All cluster operators are working fine!\n${REGULAR}"
+		else
+			echo "$DegradedOperatorsNames"
+		fi
+}
 
 ### Degraded nodes status
 function DegradedNodes {
-    omg get nodes -o wide| grep -i 'NotReady\|SchedulingDisabled'
+		DegradedNodesNames=$(omg get nodes -o wide | awk '$2!="Ready"')
+		LineCount=$(echo "$DegradedNodesNames" | wc -l)
+		if [ $LineCount -eq 1 ];
+		then
+			echo -e "\n${PURPLE}All nodes are in Ready state!\n${REGULAR}"
+		else
+			echo "$DegradedNodesNames"
+		fi
 }
 
 ### Degraded node description
 function DegradedNodesDescription {
-	DegradedNodesName=$(omg get nodes | grep -i 'NotReady\|SchedulingDisabled' | awk '{ print $1 }' | tr '\n' ' ')
-	for i in $(echo $DegradedNodesName); do
-		if [ -f $MUSTGATHER/*/cluster-scoped-resources/core/nodes/$i.yaml ];
+		DegradedNodesName=$(omg get nodes | grep -i 'NotReady\|SchedulingDisabled' | awk '{ print $1 }' | tr '\n' ' ')
+		if [ -z "$DegradedNodesName" ];
 		then
-			echo -e "\n${PURPLE}***$i***\n${REGULAR}"
-			cat $MUSTGATHER/*/cluster-scoped-resources/core/nodes/$i.yaml | sed -n '/apiVersion/,/daemonEndpoints/{ //!p }'
-			cat $MUSTGATHER/*/cluster-scoped-resources/core/nodes/$i.yaml | sed -n '/nodeInfo/,$p'
+			echo -e "\n${PURPLE}Not required as all nodes are in Ready state!\n${REGULAR}"
 		else
-			echo -e "\n***${BOLD}$i node description not found***\n${REGULAR}"
+			for i in $(echo $DegradedNodesName); do
+				if [ -f $MUSTGATHER/*/cluster-scoped-resources/core/nodes/$i.yaml ];
+				then
+					echo -e "\n${PURPLE}***$i***\n${REGULAR}"
+					cat $MUSTGATHER/*/cluster-scoped-resources/core/nodes/$i.yaml | sed -n '/apiVersion/,/daemonEndpoints/{ //!p }'
+					cat $MUSTGATHER/*/cluster-scoped-resources/core/nodes/$i.yaml | sed -n '/nodeInfo/,$p'
+				else
+					echo -e "\n***${BOLD}$i node description not found***\n${REGULAR}"
+				fi
+			done
 		fi
-	done
 }
 
-
-### machine-config-daemon pod status for degraded nodes
-function MCDPodStatus {
-	DegradedNodesName=$(omg get nodes | grep -i 'NotReady\|SchedulingDisabled' | awk '{ print "\\|" $1 }' | tr '\n' ' ' | sed "s/ //g" | sed 's/^.//' | sed 's/^.//')
-		omg get pod -o wide -n openshift-machine-config-operator | grep -i $DegradedNodesName
+### pods not in Running or Succeeded state
+function pods {
+		Pods=$(omg get pod -o wide -A | grep -Ev 'Running|Succeeded')
+		LineCount=$(echo "$Pods" | wc -l)
+		if [ $LineCount -eq 1 ];
+		then
+			echo -e "\n${PURPLE}All pods are in Running state!\n${REGULAR}"
+		else
+			echo "$Pods"
+		fi
 }
-
 
 ### machine-config-daemon pod logs for degraded nodes
 function MCDPodLogs {
-	DegradedNodesName=$(omg get nodes | grep -i 'NotReady\|SchedulingDisabled' | awk '{ print $1 }' | tr '\n' ' ')
-	for i in $(echo $DegradedNodesName); do
-		MCDPods=$(omg get pod -o wide -n openshift-machine-config-operator | grep -i $i | awk '{ print $1 }')
-		if [ -f $MUSTGATHER/*/namespaces/openshift-machine-config-operator/pods/$MCDPods/machine-config-daemon/machine-config-daemon/logs/current.log ];
+		DegradedNodesName=$(omg get nodes | grep -i 'NotReady\|SchedulingDisabled' | awk '{ print $1 }' | tr '\n' ' ')
+		if [ -z "$DegradedNodesName" ];
 		then
-			echo -e "\n${PURPLE}***$MCDPods***\n${REGULAR}"
-			cat $MUSTGATHER/*/namespaces/openshift-machine-config-operator/pods/$MCDPods/machine-config-daemon/machine-config-daemon/logs/current.log | tail -n 15
+			echo -e "\n${PURPLE}Not required as all nodes are in Ready state!\n${REGULAR}"
 		else
-			echo -e "\n***${BOLD}$MCDPods logs not found***\n${REGULAR}"
+			for i in $(echo $DegradedNodesName); do
+				MCDPods=$(omg get pod -o wide -n openshift-machine-config-operator | grep -i $i | awk '{ print $1 }')
+				if [ -f $MUSTGATHER/*/namespaces/openshift-machine-config-operator/pods/$MCDPods/machine-config-daemon/machine-config-daemon/logs/current.log ] && [ -s $MUSTGATHER/*/namespaces/openshift-machine-config-operator/pods/$MCDPods/machine-config-daemon/machine-config-daemon/logs/current.log ];
+				then
+					echo -e "\n${PURPLE}***$MCDPods pod logs for degraded $i node***\n${REGULAR}"
+					cat $MUSTGATHER/*/namespaces/openshift-machine-config-operator/pods/$MCDPods/machine-config-daemon/machine-config-daemon/logs/current.log | tail -n 15
+				else
+					echo -e "\n${PURPLE}***$MCDPods pod logs not found for degraded $i node***\n${REGULAR}"
+				fi
+			done
 		fi
-	done
 }
 
 function title {
-	echo -e "\n${BOLD}${RED}----------${1}----------\n${REGULAR}"
+		echo -e "\n${BOLD}${RED}----------${1}----------\n${REGULAR}"
 }
 
 function main {
 
-	validate
+		validate
 
-	title "Degraded Nodes"
-	DegradedNodes
+		title "ClusterVersion Details"
+		clusterversion
 
-	title "Description of Degraded Nodes"
-	DegradedNodesDescription
+		title "Degraded Operators"
+		DegradedOperators
 
-	title "machine-config-daemon pod status for degraded nodes"
-	MCDPodStatus
+		title "Degraded Nodes"
+		DegradedNodes
 
-	title "machine-config-daemon pod logs for degraded nodes"
-	MCDPodLogs
+		title "Description of Degraded Nodes"
+		DegradedNodesDescription
 
+		title "Pods not in Running state"
+		pods
+
+		title "machine-config-daemon pod logs for degraded nodes"
+		MCDPodLogs
 }
 
 main
